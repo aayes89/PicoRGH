8ead53efee19df00635e09717e4a325e897ea08e
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2025-04-04 11:28:34 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,33 @@
 #include "hardware/clocks.h"
 
 // Definición de pines
+
+#ifndef PICO_DEFAULT_LED_PIN
+#endif
+
 #define RESET_PIN 0  // Conectar al pin RST de la CPU de la Xbox 360
 #define POST_PIN  1  // Conectar a un pin POST_OUT (ej. POSTBIT 0-7) para sincronización
 
 // Parámetros del glitch (ajustables)
 #define GLITCH_DELAY_US 5000  // Retardo inicial en microsegundos antes del pulso (aproximado para Xenon)
 #define GLITCH_PULSE_NS 100   // Duración del pulso en nanosegundos (100 ns típico para RGH)
+
+// Inicializar GPIO para el LED
+void pico_led_init(void) {
+#ifdef PICO_DEFAULT_LED_PIN
+    // utiliza la funcionalidad de GPIO para encender o apagar el LED
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+#endif
+}
+
+// Encender y apagar LED
+void pico_set_led(bool led_on) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    // Establece el estado a ON/OFF
+    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+#endif
+}
 
 // Configuración inicial
 void glitch_init() {
@@ -25,9 +46,11 @@ void glitch_init() {
 // Generar un pulso de reset
 void send_glitch_pulse() {
     gpio_put(RESET_PIN, 1); // Activar reset
+    pico_set_led(true);
     //sleep_ns(GLITCH_PULSE_NS); // Duración del pulso (100 ns)
     busy_wait_at_least_cycles(GLITCH_PULSE_NS * (clock_get_hz(clk_sys) / 1000000000));
     gpio_put(RESET_PIN, 0); // Desactivar reset
+    pico_set_led(false);
 }
 
 // Monitorear POST y enviar el glitch
